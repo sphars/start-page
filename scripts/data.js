2c1fb6607483589494305be2f066a402bf58bec1
Spencer Harston <spencer314@gmail.com>
2018-04-19 16:15:01 -0600

    Adjust data
    Reorganized the data so that all bookmarks are an array under a category. The bookmarks container is now properly filled with bookmarks by category.

--- scripts/data.js
+++ scripts/data.js
@@ -1,25 +1,35 @@
-var data =
-    {
-        "bookmarks": [
-            {
-                "category": "Develop",
-                "name": "Github",
-                "location": "https://www.github.com/"
-            },
-            {
-                "category": "Develop",
-                "name": "Stack Overflow",
-                "location": "https://www.stackoverflow.com/"
-            },
-            {
-                "category": "Read",
-                "name": "Ars Technica",
-                "location": "https://www.arstechnica.com/"
-            },
-            {
-                "category": "Read",
-                "name": "The Verge",
-                "location": "https://www.theverge.com"
-            }
-        ]
-    }
+var bookmarks = 
+{
+    "categories":[
+        {
+            "category":"Develop",
+            "bookmarks":[
+                {
+                    "name": "Github",
+                    "location": "https://www.github.com/"
+                },
+                {
+                    "name": "Stack Overflow",
+                    "location": "https://www.stackoverflow.com/"
+                }
+            ]
+        },
+        {
+            "category":"Read",
+            "bookmarks":[
+                {
+                    "name": "Ars Technica",
+                    "location": "https://www.arstechnica.com/"
+                },
+                {
+                    "name": "The Verge",
+                    "location": "https://www.theverge.com"
+                },
+                {
+                    "name": "Android Police",
+                    "location": "https://www.androidpolice.com"
+                }
+            ]
+        }
+    ]
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,49 @@
 console.log("js loaded");
-console.log(data);
 
-var output = "";
+function CreateBookmarks() {
+    //get the bookmark container
+    var bookmarkContainer = document.getElementById("bookmarks");
 
-for(var i in data.bookmarks){
-    output += "<div><a href='" + data.bookmarks[i].location + "'>" + data.bookmarks[i].name + "</a></div>"
+    for (var i in bookmarks.categories) {
+        //create the category container and set the class
+        var bookmarkCategory = document.createElement('div');
+        bookmarkCategory.className = 'bookmark-category';
+
+        //create the category name element and set the class
+        var bookmarkCategoryName = document.createElement('div');
+        bookmarkCategoryName.className = 'category-name';
+
+        //get and set the name of the category
+        var categoryName = bookmarks.categories[i].category;
+        bookmarkCategoryName.innerHTML = categoryName;
+
+        bookmarkCategory.appendChild(bookmarkCategoryName);
+
+        //loop through the entries and populate the category
+        for (var bookmark in bookmarks.categories[i].bookmarks) {
+            //create the bookmark element and set the class
+            var bookmarkElement = document.createElement('div');
+            bookmarkElement.className = 'bookmark';
+
+            //create and set the link
+            var bookmarkLocation = document.createElement('a');
+            bookmarkLocation.setAttribute('href', bookmarks.categories[i].bookmarks[bookmark].location);
+
+            //set the name of the bookmark
+            var name = bookmarks.categories[i].bookmarks[bookmark].name;
+            bookmarkLocation.innerHTML = name;
+
+            //add the location to the bookmark
+            bookmarkElement.appendChild(bookmarkLocation);
+
+            //add the bookmark to the category
+            bookmarkCategory.appendChild(bookmarkElement);
+        }
+
+        //add the category to the bookmark container
+        bookmarkContainer.appendChild(bookmarkCategory);
+    }
+
 }
 
-document.getElementById("json-test").innerHTML = output;
-
-
+document.onload = CreateBookmarks();
